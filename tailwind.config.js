65040c57e2cdddbf8b65fca4d35eb155ff54b9a7
Alan Rivas <massimo18x@hotmail.com>
2023-06-02 15:12:01 -0400

    new updates

---
+++ tailwind.config.js
@@ -0,0 +1,60 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}',],
+  theme: {
+    fontSize: {
+      'title1': ['3rem', {
+        fontWeight: 700,
+      }],
+      'title2': ['2.5rem', {
+        fontWeight: 700,
+      }],
+      'title3': ['2rem', {
+        fontWeight: 700,
+      }],
+      'subtitle': ['1.5rem', {
+        fontWeight: 500,
+      }],
+      'boldBody': ['1rem', {
+        fontWeight: 600,
+      }],
+      'bodyText': ['1rem', {
+        fontWeight: 400,
+      }],
+      'smallTextBold': ['.875rem', {
+        fontWeight: 600,
+      }],
+      'smallText': ['.875rem', {
+        fontWeight: 400,
+      }],
+      'preTextB': ['.75rem', {
+        fontWeight: 600,
+      }],
+      'preText': ['.75rem', {
+        fontWeight: 400,
+      }],
+    },
+    colors: {
+      'dark-60': '#131313',
+      'dark-40': '#767474',
+      'dark-30': '#DCDCDC',
+      'dark-20': '#F4F4F4',
+      'blue-primary': '#1D6DEC',
+      'pink-primary': '#EF355E',
+      'success': '#3CCF4E',
+      'error': '#E30815',
+      'alert': '#FAB95B',
+      'blue-secondary': '#4359F1',
+      'pink-secondary': '#FF7168',
+      'yellow-primary': '#FFA466',
+      'purple-primary': '#6445F1',
+    },
+    maxWidth: {
+      'w-body': '90rem',
+      'w-table': '63.75rem',
+    },
+    fontFamily: {
+      'Inter': ['Inter'],
+     }
+}
+}
