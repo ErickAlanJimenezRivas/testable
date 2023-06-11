6c4faa1c37f9fa0918ea44093c055fdc4e51f4a5
Alan Rivas <massimo18x@hotmail.com>
2023-06-11 11:29:48 -0400

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}',],
+  content: ['./pages/**/*.{html,js}',],
   theme: {
     fontSize: {
       'title1': ['3rem', {
